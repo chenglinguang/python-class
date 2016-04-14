0d3be46e635a7a5a96d95110b2acc1886f1e5f25
Linguang Cheng <clg19881220@163.com>
2016-04-14 19:22:39 +0800

    add some defined classes in Python

--- high_level_objective.py
+++ high_level_objective.py
@@ -159,4 +159,115 @@ class Bat(Mammal,Flyable):
     pass
 
 
-class Cat(Mammal,Runnable):
+#class Cat(Mammal,Runnable):
+###
+#defined classes 
+###
+
+#__iter__ use class itself in iteration
+class Fib(object):
+    def __init__(self):
+        self.a,self.b=0,1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        self.a,self.b=self.b,self.a+self.b
+        if self.a>1000:
+            raise StopIteration();
+        return self.a
+    
+for n in Fib():
+    print(n)
+
+
+class FibA(object):
+    def __getitem__(self,n):
+        a,b=1,1
+        for x in range(n):
+            a,b=b,a+b
+        return a 
+    
+f=FibA()
+print(f[0])
+print(f[1])
+print(f[2])
+print(f[3])
+
+class StudentD():
+    def __init__(self,name):
+        self.name=name
+    def __str__(self):
+        return 'Student object (name: %s)' % self.name
+    
+astudent=StudentD('Michael')
+print(astudent)
+
+
+class FibB():
+    def __getitem__(self,n):
+        if isinstance(n,int):
+            a,b=1,1
+            for x in range(n):
+                a,b=b,a+b
+            return a 
+        if isinstance(n,slice):
+            start=n.start
+            stop=n.stop
+            if start is None:
+                start=0
+            a,b=1,1
+            L=[]
+            for x in range(stop):
+                if x >=start:
+                    L.append(a)
+                a,b=b,a+b
+            return L
+                    
+ff=FibB()
+print(ff[4])
+
+print(ff[:10])
+print(ff[1:5])
+    
+class StudentC():
+    def __init__(self):
+        self.name='Michael'           
+    def __getattr__(self,attr):
+        if attr=='score':
+            return 99
+        raise AttributeError('\'Student\' object has no attribute \'%s\'' % attr)
+        
+sss=StudentC()
+print(sss.name)
+print(sss.score)
+#print(sss.height)
+
+class StudentE():
+    def __init__(self,name):
+        self.name=name
+    
+    def __call__(self):
+        print('My name is %s.' % self.name)
+        
+ssa=StudentE('Lisa')
+ssa()
+
+####我们需要判断一个对象是否能被调用，能被调用的对象就是一个Callable对象，比如函数和我们上面定义的带有__call__()的类实例：
+print(callable(StudentE('a')))
+print(callable(max))
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
